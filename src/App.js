b9674fb51af93cea079838c1d8da45d6896bc67f
antoinegilles <antoinegillesapb@hotmail.com>
2019-02-04 10:22:08 +0100

    premiere version stable // probleme de cors

--- src/App.js
+++ src/App.js
@@ -10,41 +10,101 @@ gamerTag = null;
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {value: '',value2:"us",value3:"xbl",json:''};
+
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeRegion = this.handleChangeRegion.bind(this);
+    this.handleChangePlateform = this.handleChangePlateform.bind(this);
+    this.handleSubmitTest = this.handleSubmitTest.bind(this);
+
+  }
+
+  handleChangeName(event) {
+    this.setState({value: event.target.value});
+  }
+  handleChangeRegion(event) {
+    this.setState({value2: event.target.value});
+  }
+  handleChangePlateform(event) {
+    this.setState({value3: event.target.value});
+  }
+
+  handleSubmitTest(event) {
+    event.preventDefault();
 
     const overwatch = require('overwatch-api');
-    const platform = 'pc';
-    const region = 'us';
-    const tag =  "Calvin-1337";
+    const platform = this.state.value3;
+    const reg = this.state.value2;
+    const tag =  this.state.value;
+    console.log(platform + " " + reg)
+
   
-    overwatch.getProfile(platform, region, tag, (err, json) => {
+    overwatch.getProfile(platform, reg, tag, (err, json) => {
       if (err) console.error(err);
       else this.setState(json)
-      console.log(json)
-    });
-  
-}
-
+      console.log(this.state)
+    });  
+  }
  
+ /*
+  getProfiles(name,region,plateform){
+  const overwatch = require('overwatch-api');
+  const platform = plateform.state.platform;
+  const reg = region.target.region;
+  const tag =  name.target.name;
 
-
-handleSubmit(event) {
-  event.preventDefault();
-
+  overwatch.getProfile(platform, reg, tag, (err, json) => {
+    if (err) console.error(err);
+    else this.setState(json)
+    console.log(json)
+  });
 }
 
+handleChangeName(event) {
+  this.setState({name: event.target.value});
+}
+handleChangeRegion(event) {
+  this.setState({reg: event.target.value});
+}
+handleChangePlateform(event) {
+  this.setState({tag: event.target.value});
+}
+*/
 
   render() {
+  
+  
+
     return (
       <div className="App">
     
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" name="surname"  onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+    <form onSubmit={this.handleSubmitTest}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChangeName} />
+        </label>
+      
+        <label>
+          Select the Plateform:
+          <select value={this.state.value3} onChange={this.handleChangePlateform}>
+            <option value="pc">pc</option>
+            <option value="xbl">xbl</option>
+            <option value="psn">psn</option> 
+          </select>
+        </label>       
+        
+        <label>
+          Select the Region
+          <select value={this.state.value2} onChange={this.handleChangeRegion}>
+            <option value="us">us</option>
+            <option value="eu">eu</option>
+            <option value="kr">kr</option>
+            <option value="cn">cn</option>
+            <option value="global">global</option>
+          </select>
+        </label>
+       <input type="submit" value="Submit" />
+    </form>
 
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
